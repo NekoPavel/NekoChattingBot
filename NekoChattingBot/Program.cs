bcaec96801860256997f40fd2b5f16acf02c75e4
NekoPavel <p.kuzminov00@gmail.com>
2022-04-09 12:13:59 +0200

    Fixed initial time

--- NekoChattingBot/Program.cs
+++ NekoChattingBot/Program.cs
@@ -17,7 +17,7 @@ namespace NekoChattingBot
     {
         static async Task Main(string[] args)
         {
-            DateTime lastSeen = DateTime.Now;
+            DateTime lastSeen = DateTime.MinValue;
             string mods = "ThatOneGuyWhoSpamsPogpega, aeonim, andros18, bennyoberwinch, bigtimemassivecash, binfy, bluecrystal004, boxbox, chase55t, chrchie, cloud9, deadrote, derpyfoxplayz, digitalhypno, dios_dong, doremy, elcheer, eleeement_, emanfman, enyoti, fossabot, happystick, honmi, intlcoco, itswinter, jamiethebull, joiechii, kaoran, kerneon, ksn24, l3lackshark, littleendu, luckfire, mikuia, mrchompysaur, mrdutchboi, mrnolife_, nightbot, olibomby, oralekin, piscator_, rainbowmeeps, ryotou, shigetora, shugi, slpchatbot, soarnathan, soran2202, streamelements, stunterletsplay, thepoon, theramu, therealzachtv, toekneered, toybickler, tru3o_o, tuonto, warpworldbot, wholewheatpete, yazzehh, ypaperr, zarrah, zonelouise, nekopavel";
             string password = Environment.GetEnvironmentVariable("TWITCH_OAUTH", EnvironmentVariableTarget.User);
             string botUsername = "NekoChattingBot";
