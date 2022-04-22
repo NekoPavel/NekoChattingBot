772703d48252a1c2d12ae13c45dc1038eca386a6
Mikhail <jackglowpetux@gmail.com>
2022-04-22 16:25:10 +0500

    Add test command

---
+++ NekoChattingBot/Commands/Test.cs
@@ -0,0 +1,12 @@
+﻿namespace NekoChattingBot.Commands
+{
+    public class Test : Command
+    {
+        public static string Name = "test";
+
+        public override async void Execute(TwitchBot botInstance, string rawArgs)
+        {
+            await botInstance.SendMessage("pancakes", "Test!");
+        }
+    }
+}
