4d8cdee5bb67a4a89a27768de88e1892c9a305c9
Mikhail <jackglowpetux@gmail.com>
2022-04-22 16:24:33 +0500

    Add command implementation

---
+++ NekoChattingBot/Command.cs
@@ -0,0 +1,18 @@
+﻿namespace NekoChattingBot
+{
+    /// <summary>
+    /// Twitch bot command.
+    /// </summary>
+    public abstract class Command
+    {
+        /// <summary>
+        /// The name of the command (lowercase, without prefix), that will be used to execute it. Example: `skin`.
+        /// </summary>
+        public string ExecutionName;
+        /// <summary>
+        /// Method that will be executed, once command is fired. Should be async if any interaction with <see cref="TwitchBot"/> is planned.
+        /// </summary>
+        /// <param name="rawArgs">Raw unsplit string of arguments provided after the command.</param>
+        public abstract void Execute(TwitchBot botInstance, string rawArgs);
+    }
+}
